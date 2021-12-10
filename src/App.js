c688f72259b71bffd1e1f4c5bc7e62bd034deca1
Chetha <vuthychetha@gmail.com>
2021-12-10 23:50:59 +0700

    test commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
 const inventory = {
   a: { name: '🧉 Yerba Mate', price: 10 },
   b: { name: '☕️ Coffee', price: 15 },
-  c: { name: '🍵 Tea', price: 7.5 },
+  c: { name: '🍵 Tea Test', price: 7.5 },
 };
 
 const destinations = { US: 25, CA: 35, CO: 45 };
